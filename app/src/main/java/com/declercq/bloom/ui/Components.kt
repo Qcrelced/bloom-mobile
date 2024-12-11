1a6f608a6df94519c826dbfffbf070637e685d98
Qcrelced <declercq1103@gmail.com>
2024-12-11 18:53:31 +0100

    Add Buttons

--- app/src/main/java/com/declercq/bloom/ui/Components.kt
+++ app/src/main/java/com/declercq/bloom/ui/Components.kt
@@ -1,4 +1,4 @@
-package com.declercq.bloom.ui;
+package com.declercq.bloom.ui
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
@@ -11,8 +11,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,9 +42,9 @@ import com.declercq.bloom.ui.theme.BloomTheme
 @Composable
 fun SearchMovieRowItem(modifier : Modifier = Modifier, movie : MovieBean){
 
-    Column(modifier = Modifier.fillMaxWidth().height(125.dp)) {
+    Column(modifier = modifier.height(125.dp)) {
 
-        Row(modifier = modifier.fillMaxWidth()
+        Row(modifier = Modifier.fillMaxWidth()
             .background(Color.Black)
             .padding(12.dp),
             verticalAlignment = Alignment.CenterVertically){
@@ -55,10 +60,13 @@ fun SearchMovieRowItem(modifier : Modifier = Modifier, movie : MovieBean){
             )
 
             Column(modifier = Modifier.fillMaxSize()
-                .padding(12.dp, 0.dp)){
-                Text(text = movie.title, color = Color.LightGray, fontSize = 24.sp, fontWeight = FontWeight.Bold, maxLines = 3)
-                Spacer(modifier = modifier.weight(1F))
-                Row(modifier = modifier.fillMaxWidth(),
+                .padding(start = 12.dp)){
+                Row(modifier = Modifier.fillMaxWidth()){
+                    Text(modifier = Modifier.weight(1F).padding(end = 10.dp), text = movie.title, color = Color.LightGray, fontSize = 24.sp, fontWeight = FontWeight.Bold, maxLines = 3)
+                    LikeButtonItem()
+                }
+                Spacer(modifier = Modifier.weight(1F))
+                Row(modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.Bottom){
                     Text(text = movie.year, color = Color.LightGray)
@@ -67,10 +75,95 @@ fun SearchMovieRowItem(modifier : Modifier = Modifier, movie : MovieBean){
             }
         }
 
-        HorizontalDivider(modifier = modifier, thickness = 2.dp, color = Color.Red)
+        HorizontalDivider(thickness = 2.dp, color = Color.Red)
 
     }
 
+}
+
+@Composable
+fun LikeButtonItem(modifier: Modifier = Modifier){
+    IconButton( onClick = { },
+        modifier = Modifier
+            .size(52.dp)
+            .background(color = Color.DarkGray, shape = CircleShape)
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.ribbon_svgrepo_com),
+            contentDescription = "Favorite",
+            tint = Color.LightGray,
+            modifier = Modifier.size(34.dp)
+        )
+    }
+}
+
+@Composable
+fun WatchLaterButtonItem(modifier: Modifier = Modifier){
+    IconButton( onClick = { },
+        modifier = Modifier
+            .size(52.dp)
+            .background(color = Color.DarkGray, shape = CircleShape)
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.clock_svgrepo_com),
+            contentDescription = "Watch later",
+            tint = Color.LightGray,
+            modifier = Modifier.size(34.dp)
+        )
+    }
+}
+
+@Composable
+fun DetailButtonItem(modifier: Modifier = Modifier){
+    IconButton( onClick = { },
+        modifier = Modifier
+            .size(52.dp)
+            .background(color = Color.DarkGray, shape = CircleShape)
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.eye_svgrepo_com),
+            contentDescription = "Detail movie",
+            tint = Color.LightGray,
+            modifier = Modifier.size(34.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DetailButtonItemPreview(modifier: Modifier = Modifier){
+    BloomTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            DetailButtonItem(
+                modifier = Modifier.padding(innerPadding),
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun WatchLaterButtonItemPreview(modifier: Modifier = Modifier){
+    BloomTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            WatchLaterButtonItem(
+                modifier = Modifier.padding(innerPadding),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LikeButtonItemPreview(modifier: Modifier = Modifier){
+    BloomTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            LikeButtonItem(
+                modifier = Modifier.padding(innerPadding),
+            )
+        }
+    }
 }
 
 @Preview
@@ -81,7 +174,7 @@ fun SearchMovieRowItemPreview() {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             SearchMovieRowItem(
                 modifier = Modifier.padding(innerPadding),
-                MovieBean(title = "Qu'est-ce qu'on a encore fait au Bon Dieu ?", year = "2017", poster = "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg", type = "film", imdbID = "dlkslkf")
+                MovieBean(title = "Avengers d;f,klqsd dkflqsdkfj lskdfkl : endgame", year = "2017", poster = "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg", type = "film", imdbID = "tt4154796")
             )
         }
     }
